045822bf62413ac302081d00ac8398ef9bfd7cad
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-04-10 17:08:01 -0300

    Deixandos as paginas dinamicas

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 
+const videos = require("./data")
+
 const server = express()
 
 server.use(express.static('../public'))
@@ -8,15 +10,28 @@ server.use(express.static('../public'))
 server.set("view engine", "njk")
 
 nunjucks.configure("../views", {
-  express: server
+  express: server,
+  autoescape: false,
 })
 
-server.get("/", (request, response) => {
-  return response.render("about")
+server.get("/", (req, res) => {
+  const about = {
+    avatar_url: "https://avatars3.githubusercontent.com/u/6643122?s=460&v=4",
+    name: "Mayk Brito",
+    role: "Instrutor - Rocketseat",
+    description: 'Programador Full-stack, focado em trazer o melhor ensino para iniciantes em programação. Colaborador da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+    links: [
+      { name: "Github", url: "https://github.com/maykbrito" },
+      { name: "Twitter", url: "https://twitter.com/maykbrito" },
+      { name: "Linkedin", url: "https://www.linkedin.com/in/maykbrito/" },
+    ]
+  }
+  
+  return res.render("about", { about })
 })
 
-server.get("/portfolio", (request, response) => {
-  return response.render("portfolio")
+server.get("/portfolio", (req, res) => {
+  return res.render("portfolio", { items: videos })
 })
 
 server.listen(5000, () => {
